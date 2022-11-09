a39ea26c6f804a781cb9ad58ae09393c1b541f44
Restyled.io <commits@restyled.io>
2022-11-09 04:04:01 +0000

    Restyled by clang-format

--- src/App.js
+++ src/App.js
@@ -1,43 +1,42 @@
-import React, { useEffect } from 'react'
-import { Route, Routes } from "react-router-dom";
 import './App.css';
 
-import Home from './views/Home'
-import GameBoard from './views/GameBoard'
-import Register from './views/Register'
-import PlayerBoard from './views/PlayerBoard'
-import MasterPrizes from './views/MasterPrizes';
-
+import React, {useEffect} from 'react'
+import {connect} from 'react-redux'
+import {Route, Routes} from "react-router-dom";
 // import socketIOClient from "socket.io-client";
-import { io } from "socket.io-client";
-import { connect } from 'react-redux'
+import {io} from "socket.io-client";
+
 import * as gameActions from './store/gameState/gameState.actions'
-
-
-
+import GameBoard from './views/GameBoard'
+import Home from './views/Home'
+import MasterPrizes from './views/MasterPrizes';
+import PlayerBoard from './views/PlayerBoard'
+import Register from './views/Register'
 
 const LOCAL = false
 
+function App(props) {
+  const {setIoSocket} = props
 
-function App (props) {
-  const { setIoSocket } = props
-
-  const ENDPOINT = LOCAL ? 'http://localhost:9001' : "https://nicus-dirty-christmas.herokuapp.com/";
+  const ENDPOINT = LOCAL ? 'http://localhost:9001'
+                         : "https://nicus-dirty-christmas.herokuapp.com/";
 
   useEffect(() => {
     const socket = io(ENDPOINT);
     setIoSocket(socket)
-    
-  }, [ENDPOINT, setIoSocket]);
+  }, [ ENDPOINT, setIoSocket ]);
 
   return (
     <div className="App">
       <Routes>
-        <Route path = '/' element = {<Home />}/>
+        <Route path = '/' element = {
+<Home />}/>
         <Route path = '/gameboard' element = {<GameBoard />}/>
-        <Route path = '/register' element = {<Register />} />
+        <Route path = '/register' element = {
+<Register />} />
         <Route path = '/playerboard' element = {<PlayerBoard />} />
-        <Route path = '/masterprizes' element = {<MasterPrizes />} />
+        <Route path = '/masterprizes' element = {
+<MasterPrizes />} />
       </Routes>
     </div>
   );
--- src/store/gameState/gameState.actions.js
+++ src/store/gameState/gameState.actions.js
@@ -1,126 +1,117 @@
-import * as gameTypes from './gameState.types';
-import * as playerTypes from '../playerState/playerState.types'
 import axios from 'axios'
+
+import * as playerTypes from '../playerState/playerState.types'
+
+import * as gameTypes from './gameState.types';
 
 const USE_LOCAL = false
 
-const START_GAME_EP =  USE_LOCAL ? 'http://localhost:9001/game/startGame' : 'https://nicus-dirty-christmas.herokuapp.com/game/startGame'
-const REJOIN_GAME_EP = USE_LOCAL ? 'http://localhost:9001/game/rejoinGame' : 'https://nicus-dirty-christmas.herokuapp.com/game/rejoinGame'
-const SET_PLAYER_ORDER_EP = USE_LOCAL ? 'http://localhost:9001/game/setPlayerOrder' : 'https://nicus-dirty-christmas.herokuapp.com/game/setPlayerOrder'
-const GET_GAME_DATA_EP = USE_LOCAL ? 'http://localhost:9001/game/getGameData' : 'https://nicus-dirty-christmas.herokuapp.com/game/getGameData'
-const SET_PLAYER_TURN_EP =  USE_LOCAL ? 'http://localhost:9001/game/setPlayerTurn': 'https://nicus-dirty-christmas.herokuapp.com/game/setPlayerTurn'
+const START_GAME_EP =
+    USE_LOCAL ? 'http://localhost:9001/game/startGame'
+              : 'https://nicus-dirty-christmas.herokuapp.com/game/startGame'
+const REJOIN_GAME_EP =
+    USE_LOCAL ? 'http://localhost:9001/game/rejoinGame'
+              : 'https://nicus-dirty-christmas.herokuapp.com/game/rejoinGame'
+const SET_PLAYER_ORDER_EP =
+    USE_LOCAL
+        ? 'http://localhost:9001/game/setPlayerOrder'
+        : 'https://nicus-dirty-christmas.herokuapp.com/game/setPlayerOrder'
+const GET_GAME_DATA_EP =
+    USE_LOCAL ? 'http://localhost:9001/game/getGameData'
+              : 'https://nicus-dirty-christmas.herokuapp.com/game/getGameData'
+const SET_PLAYER_TURN_EP =
+    USE_LOCAL ? 'http://localhost:9001/game/setPlayerTurn'
+              : 'https://nicus-dirty-christmas.herokuapp.com/game/setPlayerTurn'
 
-
-
-export const startGame = () => async (dispatch) => {
-    try {
-        const res = await axios.post(START_GAME_EP, { data: {message: 'startGame'}})
+export const startGame =
+    () => async (dispatch) => {
+      try {
+        const res =
+            await axios.post(START_GAME_EP, {data : {message : 'startGame'}})
         const gameData = res.data.message
 
         localStorage.setItem('gameId', gameData.game_id)
         localStorage.setItem('gameStatus', gameData.game_status)
         localStorage.setItem('gameStatus', true)
-        dispatch({
-            type: gameTypes.START_GAME,
-            payload: true
-        })
+        dispatch({type : gameTypes.START_GAME, payload : true})
 
-        dispatch({
-            type: gameTypes.SET_GAME_DATA,
-            payload: gameData
-        })
+        dispatch({type : gameTypes.SET_GAME_DATA, payload : gameData})
 
         return gameData
 
-
-    } catch (error) {
+      } catch (error) {
         console.log('gameStateActions startGame Error: ', error)
+      }
     }
-}
 
-export const setGameData = (gameData) => async (dispatch) => {
-    try {
-        dispatch({
-            type: gameTypes.SET_GAME_DATA,
-            payload: gameData
-        })
-    } catch (error) {
+export const setGameData =
+    (gameData) => async (dispatch) => {
+      try {
+        dispatch({type : gameTypes.SET_GAME_DATA, payload : gameData})
+      } catch (error) {
         console.log('gameStateActions setGameData Error: ', error)
+      }
     }
-}
 
-export const refreshGameData = (gameId) => async (dispatch) => {
-    try {
-        const res = await axios.post(GET_GAME_DATA_EP, { data: { gameId }})
+export const refreshGameData =
+    (gameId) => async (dispatch) => {
+      try {
+        const res = await axios.post(GET_GAME_DATA_EP, {data : {gameId}})
         const gameData = res.data.message
 
-        if (gameData){
-            dispatch({
-                type: gameTypes.SET_GAME_DATA,
-                payload: gameData
-            })
-            return
+        if (gameData) {
+          dispatch({type : gameTypes.SET_GAME_DATA, payload : gameData})
+          return
         }
-        dispatch({
-            type: gameTypes.SET_GAME_DATA,
-            payload: null
-        })
+        dispatch({type : gameTypes.SET_GAME_DATA, payload : null})
 
-    } catch (error) {
+      } catch (error) {
         console.log('gameStateActions refreshGameData Error: ', error)
+      }
     }
-}
 
-export const setLocalCurrentTurn = (currentTurn) => async (dispatch) => {
-    try {
-        dispatch({
-            type: gameTypes.SET_CURRENT_TURN,
-            payload: currentTurn
-        })
-    } catch (error) {
+export const setLocalCurrentTurn =
+    (currentTurn) => async (dispatch) => {
+      try {
+        dispatch({type : gameTypes.SET_CURRENT_TURN, payload : currentTurn})
+      } catch (error) {
         console.log('gameStateActions setLocalCurrentTurn Error: ', error)
+      }
     }
-}
 
+export const setCurrentTurn =
+    (playerId, gameId) => async (dispatch) => {
+      try {
 
-export const setCurrentTurn = (playerId, gameId) => async (dispatch) => {
-    try{
+        await axios.post(SET_PLAYER_TURN_EP, {data : {playerId, gameId}})
 
-        await axios.post(SET_PLAYER_TURN_EP, {data: {playerId, gameId}})
-
-        dispatch({
-            type: gameTypes.SET_CURRENT_TURN,
-            payload: playerId
-        })
-    } catch (error) {
+        dispatch({type : gameTypes.SET_CURRENT_TURN, payload : playerId})
+      } catch (error) {
         console.log('gameStateActions setCurrentTurn Error: ', error)
-
+      }
     }
-}
 
-export const rejoinGame = (userName, pin) => async (dispatch) => {
-    const rejoinRes = await axios.post(REJOIN_GAME_EP, { data: {userName, pin}})
-    console.log('rejoinRes: ', rejoinRes.message) //!REMOVE
-    const { 
-            playerData, 
-            playerOrder, 
-            gameData 
-        } = rejoinRes.data.message
+export const rejoinGame =
+    (userName, pin) => async (dispatch) => {
+      const rejoinRes =
+          await axios.post(REJOIN_GAME_EP, {data : {userName, pin}})
+      console.log('rejoinRes: ', rejoinRes.message) //! REMOVE
+      const {playerData, playerOrder, gameData} = rejoinRes.data.message
 
+      if (rejoinRes.data.message === 401) return false
 
-    if(rejoinRes.data.message === 401) return false
+      console.log('playerData: ', playerData)   //! REMOVE
+      console.log('playerOrder: ', playerOrder) //! REMOVE
+      console.log('gameData: ', gameData)       //! REMOVE
 
-    console.log('playerData: ', playerData) //!REMOVE
-    console.log('playerOrder: ', playerOrder) //!REMOVE
-    console.log('gameData: ', gameData) //!REMOVE
-
-    if(!gameData){
+      if (!gameData) {
         localStorage.setItem('playerData', JSON.stringify(playerData))
-    }else{
-        localStorage.setItem('playerData', JSON.stringify(playerData))
-        localStorage.setItem('shuffledPlayers', JSON.stringify(gameData.game_order))
-        localStorage.setItem('gameId', gameData.game_id)
-        localStorage.setItem('gameData', JSON.stringify(gameData))
+      }
+      else {localStorage.setItem('playerData', JSON.stringify(playerData))
+      localStorage.setItem('shuffledPlayers',
+                           JSON.stringify(gameData.game_order))
+      localStorage.setItem('gameId', gameData.game_id)
+      localStorage.setItem('gameData', JSON.stringify(gameData))
         localStorage.setItem('currentTurn', gameData.current_turn)
     }
 
@@ -130,28 +121,21 @@ export const rejoinGame = (userName, pin) => async (dispatch) => {
         payload: playerData
     })
 
-    return true
+        return true
+    }
 
-}
-
-export const setIoSocket = (socket) => async (dispatch) => {
-    dispatch({
-        type: gameTypes.SET_SOCKET,
-        payload: socket
-    })
-}
+export const setIoSocket = (socket) => async (
+    dispatch) => { dispatch({type : gameTypes.SET_SOCKET, payload : socket})}
 
 export const setPlayerOrder = (playerArray, gameId) => async (dispatch) => {
-    try {
-        console.log('gameState.actions gameId: ', gameId) //! REMOVE
-        const res = await axios.post(SET_PLAYER_ORDER_EP, { data: {playerArray, gameId}})
-        const playerOrder = res.data.message
+  try {
+    console.log('gameState.actions gameId: ', gameId) //! REMOVE
+    const res =
+        await axios.post(SET_PLAYER_ORDER_EP, {data : {playerArray, gameId}})
+    const playerOrder = res.data.message
 
-        dispatch({
-            type: gameTypes.SET_PLAYER_ORDER,
-            payload: playerOrder
-        })
-    } catch (error) {
-        console.log('gameStateActions setPlayerOrder Error: ', error)
-    }
+    dispatch({type : gameTypes.SET_PLAYER_ORDER, payload : playerOrder})
+  } catch (error) {
+    console.log('gameStateActions setPlayerOrder Error: ', error)
+  }
 }
